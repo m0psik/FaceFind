d7f3eed512d989ce8168543e14e44d19148ed67c
filalex228 <filippov-sasha2000@mail.ru>
2022-09-20 17:42:58 +0300

    Сериализация данных о лицах с помощью протобуфера

--- src/libproto/FaceInfo.cpp
+++ src/libproto/FaceInfo.cpp
@@ -1,1 +1,42 @@
 #include "FaceInfo.h"
+#include "face.pb.h"
+
+FaceInfo::FaceInfo() {
+    this->direction = DIRECTION::IN;
+    this->uuid = QUuid::createUuid();
+}
+
+void
+FaceInfo::addFace(const QByteArray &face) {
+    this->faces.insert(face);
+}
+
+void
+FaceInfo::setDirection(const DIRECTION &_direction) {
+    this->direction = _direction;
+}
+
+QUuid
+FaceInfo::getUuid() {
+    return this->uuid;
+}
+
+QByteArray
+FaceInfo::getSerializedData() {
+    return this->serializedData;
+}
+
+void FaceInfo::serialize() {
+    Data *data = new Data();
+    data->mutable_person()->set_uuid(this->uuid.toByteArray().data());
+    data->mutable_person()->set_direction(static_cast< Direction > ( this->direction ));
+    int it = 0;
+    for (QByteArray face: this->faces) {
+        ++it;
+        data->mutable_person()->mutable_faces(it)->set_face_data(face.data());
+        data->mutable_person()->mutable_faces(it)->set_num(it);
+    }
+    this->serializedData = data->SerializeAsString().data();
+}
+
+
--- src/libproto/FaceInfo.h
+++ src/libproto/FaceInfo.h
@@ -1,10 +1,29 @@
 #ifndef FACEFIND_FACEINFO_H
 #define FACEFIND_FACEINFO_H
 
+#include <set>
+#include <QUuid>
 
-class FaceInfo {
-
+enum DIRECTION {
+    IN  = 0,
+    OUT = 1
 };
 
+class FaceInfo {
+    FaceInfo();
+
+    void addFace(const QByteArray& face);
+    void setDirection(const DIRECTION& _direction);
+    void serialize();
+
+    QUuid getUuid();
+    QByteArray getSerializedData();
+
+private:
+    DIRECTION direction;
+    std::set< QByteArray > faces;
+    QUuid uuid;
+    QByteArray serializedData;
+};
 
 #endif //FACEFIND_FACEINFO_H
